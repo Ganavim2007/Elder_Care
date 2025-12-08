b7a5d4866bef9b2e6b75d178ceea1ea15ab25ae3
Ganavim2007 <ganavim2007@gmail.com>
2025-12-08 20:39:39 +0530

    Update App.js

--- App.js
+++ App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
+
+import LoginScreen from './screens/LoginScreen';
+import SignupScreen from './screens/SignupScreen';
 import HomeScreen from './screens/HomeScreen';
 import SeniorDashboard from './screens/SeniorDashboard';
 import CaregiverDashboard from './screens/CaregiverDashboard';
